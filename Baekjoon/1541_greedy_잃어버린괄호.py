1c5da50c67e30f46a8f0efb869f02dd5a8f2690b
kty6278 <kty6278@naver.com>
2021-08-15 22:30:30 +0900

    0815-1

---
+++ Baekjoon/1541_greedy_잃어버린괄호.py
@@ -0,0 +1,15 @@
+math_ex = input().split("-")
+
+
+start = 0
+
+# 첫시작은 + 이고, 이후 +인 경우 해당
+for math in math_ex[0].split("+"):
+    start += int(math)
+# print(start)
+
+# -인 경우
+for my in range(1, len(math_ex)):
+    for my_2 in math_ex[my].split("+"):
+        start -= int(my_2)
+print(start)
