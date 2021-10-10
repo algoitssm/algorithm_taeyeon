832b649a005b6e17f8318cb241c7e8b68074f97f
kty6278 <kty6278@naver.com>
2021-10-11 01:18:55 +0900

    1011

--- Baekjoon/2448_분할정복_별찍기-11.py
+++ Baekjoon/2448_분할정복_별찍기-11.py
@@ -2,10 +2,19 @@ import sys
 sys.stdin = open('input.txt')
 
 n = int(input())
-for i in range(n):
-    if i // 3 == 0:
-        print(' '+'*'+' ')
-    elif i // 3 == 1:
-        print('* *')
-    elif i// 3 == 2:
-        print('***** ' * (n//3))''
+star = ["  *  ", " * * ", "*****"]
+n_list = [3*(2**i) for i in range(11)]
+# print(n_list) # [3, 6, 12, 24, 48, 96, 192, 384, 768, 1536, 3072]
+# print(pow)
+k = n_list.index(n)
+for _ in range(k):
+    c = []
+    for i in range(len(star)*2):
+        m = len(star)
+        if i < m:
+            c.append(' '*(len(star[i % m])//2 + 1)+star[i % m]+' '*(len(star[i % m])//2+1))
+        else:
+            c.append(star[i % m]+' '+star[i % m])
+    star = c
+for s in star:
+    print(s)
